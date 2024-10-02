f068382a5f843c01f43e6e98fccd1f0bba082fcc
jsusmachaca <falcorgd@gmail.com>
2024-10-02 09:49:30 -0500

    fix var name

--- worker/pkg/movie/service/api.go
+++ worker/pkg/movie/service/api.go
@@ -9,11 +9,11 @@ import (
 )
 
 func ApiService(body io.Reader) error {
-	API_URL := os.Getenv("PROCESSOR_URL")
+	PROCESSOR_URL := os.Getenv("PROCESSOR_URL")
 
 	client := &http.Client{}
 
-	res, err := client.Post(API_URL+"/recommend", "application/json", body)
+	res, err := client.Post(PROCESSOR_URL+"/recommend", "application/json", body)
 	if err != nil {
 		return err
 	}
