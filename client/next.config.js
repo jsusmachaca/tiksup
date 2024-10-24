13e614f245adcd27b126ce6b1802d750b0ad1e6c
jsusmachaca <falcorgd@gmail.com>
2024-10-24 17:12:53 -0500

    update dependencies

--- client/next.config.js
+++ client/next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   env: {
     GATEWAY_URL: process.env.GATEWAY_URL,
   },
+  images: {
+    domains: ['gqldqjebnxaxmslrgqwf.supabase.co'],
+  },
 };
 
 module.exports = nextConfig;
