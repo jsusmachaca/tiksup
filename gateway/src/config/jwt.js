d94bba2bd0495736bf62ce4cc1eca991bba9b1d2
jhoe-y <jhoeyauricasa@gmail.com>
2024-11-10 01:36:06 -0500

    Generated token

--- gateway/src/config/jwt.js
+++ gateway/src/config/jwt.js
@@ -1,6 +1,8 @@
 import jwt from 'jsonwebtoken'
 import 'dotenv/config'
 
+export const generateToken = (data) => jwt.sign(data, process.env.SECRET_KEY, { expiresIn: '1y' })
+
 export const validateToken = (token) => {
   try {
     const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/jsusmachaca/tiksup.git
+
+cd tiksup
+
+docker compose up --build -d
